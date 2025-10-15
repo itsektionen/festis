a2e3b701777067473460275ef18f434af7922c69
epicmeme <simonculus@gmail.com>
2025-10-15 21:48:05 +0200

    add api versioning

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from scalar_fastapi import get_scalar_api_reference, Theme
 
-
-from app.fest import generate_pdf
-from app.models import Festanmalan
+from app.v1.routers import v1_router
 
 app = FastAPI(
     title="Festis",
     description="API for generating festanmälan .pdf's",
     docs_url=None,
     openapi_url="/openapi.json",
+    version="0.1.0",
 )
 
 app.mount("/img", StaticFiles(directory="static/img"), name="img")
+app.include_router(v1_router)
 
 
 @app.get("/", include_in_schema=False)
@@ -24,18 +24,4 @@ def index():
         title=app.title,
         theme=Theme.SATURN,
         scalar_favicon_url="img/favicon.png",
-    )
-
-
-@app.post(
-    "/create",
-    name="Create festanmälan",
-    description="This endpoint creates a festanmälan .pdf with the data provided",
-)
-def festanmalan(anmalan: Festanmalan):
-    pdf = generate_pdf(anmalan)
-    return StreamingResponse(
-        pdf,
-        media_type="application/pdf",
-        headers={"Content-Disposition": f"attachment; filename={anmalan.date}.pdf"},
     )
